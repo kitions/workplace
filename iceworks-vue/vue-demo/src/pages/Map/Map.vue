a1eca7e47c047b8de697d66e00d4376ff5c7b3b6
link <nsplknsplk@qq.com>
2018-08-27 15:35:55 +0800

    ice-works  后台测试

---
+++ iceworks-vue/vue-demo/src/pages/Map/Map.vue
@@ -0,0 +1,16 @@
+<template>
+  <div className="map-page">
+        <MapToggleChart />
+      </div>
+</template>
+
+<script>
+import MapToggleChart from './components/MapToggleChart';
+
+export default {
+  name: 'Map',
+  components: {
+    MapToggleChart,
+  },
+};
+</script>
---
+++ iceworks-vue/vue-demo/src/pages/Test/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div className="test-page">
+        <TabTable />
+        <DataDisplay />
+        <DataDisplay1 />
+        <SimpleFooter />
+      </div>
+</template>
+
+<script>
+import TabTable from './components/TabTable';
+import BasicForm from './components/BasicForm';
+import DataDisplay from './components/DataDisplay';
+import DataDisplay1 from './components/DataDisplay1';
+import SimpleFooter from './components/SimpleFooter';
+
+export default {
+  name: 'Test',
+  components: {
+    TabTable,
+    BasicForm,
+    DataDisplay,
+    DataDisplay1,
+    SimpleFooter,
+  },
+};
+</script>
